3350897d48f317ffb9ac4df9c152623a22758dcc
Saurav Kulshrestha <sauravkul0@gmail.com>
2021-07-20 16:48:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+
+function toggleHide() {
+    let btn = document.getElementById('btn1');
+    let discription = document.getElementById('discription1');
+    if (discription.style.display != 'none') {
+        discription.style.display = 'none';
+        btn1.innerHTML = '+';
+    }
+    else {
+        discription.style.display = 'block';
+        btn1.innerHTML = 'x';
+    }
+}
+
+function toggleHide2() {
+    let btn = document.getElementById('btn2');
+    let discription = document.getElementById('discription2');
+    if (discription.style.display != 'none') {
+        discription.style.display = 'none';
+        btn2.innerHTML = '+';
+    }
+    else {
+        discription.style.display = 'block';
+        btn2.innerHTML = 'x';
+    }
+}
+function toggleHide3() {
+    let btn = document.getElementById('btn3');
+    let discription = document.getElementById('discription3');
+    if (discription.style.display != 'none') {
+        discription.style.display = 'none';
+        btn3.innerHTML = '+';
+    }
+    else {
+        discription.style.display = 'block';
+        btn3.innerHTML = 'x';
+    }
+}
+
+function toggleHide4() {
+    let btn = document.getElementById('btn4');
+    let discription = document.getElementById('discription4');
+    if (discription.style.display != 'none') {
+        discription.style.display = 'none';
+        btn4.innerHTML = '+';
+    }
+    else {
+        discription.style.display = 'block';
+        btn4.innerHTML = 'x';
+    }
+}
+function toggleHide5() {
+    let btn = document.getElementById('btn5');
+    let discription = document.getElementById('discription5');
+    if (discription.style.display != 'none') {
+        discription.style.display = 'none';
+        btn5.innerHTML = '+';
+    }
+    else {
+        discription.style.display = 'block';
+        btn5.innerHTML = 'x';
+    }
+}
+function toggleHide6() {
+    let btn = document.getElementById('btn6');
+    let discription = document.getElementById('discription6');
+    if (discription.style.display != 'none') {
+        discription.style.display = 'none';
+        btn6.innerHTML = '+';
+    }
+    else {
+        discription.style.display = 'block';
+        btn6.innerHTML = 'x';
+    }
+}
